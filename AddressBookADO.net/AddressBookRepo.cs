70ce66fcae65b4e68126d4994c901150f8fcaa1e
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-11-06 16:17:06 +0530

    [Kartikeya] UC added to establish a connection between the database and our code

--- AddressBookADO.net/AddressBookRepo.cs
+++ AddressBookADO.net/AddressBookRepo.cs
@@ -19,5 +19,51 @@ namespace AddressBookADO.net
         /// </summary>
         public static string connectionString = "Data Source=KARTIKEYA;Initial Catalog=addressBookService;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         SqlConnection connection = new SqlConnection(connectionString);
+        /// <summary>
+        /// UC 2:
+        /// Gets all the Entries from the database.
+        /// </summary>
+        /// <exception cref="Exception"></exception>
+        public void GetAllEntries()
+        {
+            AddressBookModel model = new AddressBookModel();
+            try
+            {
+                using (this.connection)
+                {
+                    string query = @"select * from address_book";
+                    SqlCommand command = new SqlCommand(query, this.connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            model.FirstName = reader.GetString(0);
+                            model.LastName = reader.GetString(1);
+                            model.Address = reader.GetString(2);
+                            model.City = reader.GetString(3);
+                            model.State = reader.GetString(4);
+                            model.Zip = reader.GetInt32(5);
+                            model.PhoneNo = reader.GetInt64(6);
+                            model.Email = reader.GetString(7);
+                            model.AddressBookName = reader.GetString(8);
+                            model.ContactType = reader.GetString(9);
+                            Console.WriteLine("{0} {1}", model.FirstName, model.LastName);
+                        }
+                    }
+                    else
+                        Console.WriteLine("No data found");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
     }
 }
--- AddressBookADO.net/Program.cs
+++ AddressBookADO.net/Program.cs
@@ -13,6 +13,10 @@ namespace AddressBookADO.net
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to address book problem ADO.net");
+            /// Creating an object of AddressBookRepo class
+            AddressBookRepo addressBookRepo = new AddressBookRepo();
+            /// Calling the GetAllEntries method from AddressBookRepo class
+            addressBookRepo.GetAllEntries();
         }
     }
 }
