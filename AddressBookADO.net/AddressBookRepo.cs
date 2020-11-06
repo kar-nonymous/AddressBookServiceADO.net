6e639c9c80d04ce750c65a71d764ea4751cd53b9
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-11-06 16:17:13 +0530

    [Kartikeya] UC added to retrieve contacts by city or state name

--- AddressBookADO.net/AddressBookRepo.cs
+++ AddressBookADO.net/AddressBookRepo.cs
@@ -171,5 +171,50 @@ namespace AddressBookADO.net
                 connection.Close();
             }
         }
+        /// <summary>
+        /// UC 6:
+        /// Retrieves data from table with given name
+        /// </summary>
+        public void GetPersonByCityOrState()
+        {
+            AddressBookModel model = new AddressBookModel();
+            try
+            {
+                using (this.connection)
+                {
+                    string query = @"select * from address_book where City='Agra' or State='Uttar Pradesh'";
+                    SqlCommand command = new SqlCommand(query, this.connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            model.FirstName = reader.GetString(0);
+                            model.LastName = reader.GetString(1);
+                            model.Address = reader.GetString(2);
+                            model.City = reader.GetString(3);
+                            model.State = reader.GetString(4);
+                            model.Zip = reader.GetInt32(5);
+                            model.PhoneNo = reader.GetInt64(6);
+                            model.Email = reader.GetString(7);
+                            model.AddressBookName = reader.GetString(8);
+                            model.ContactType = reader.GetString(9);
+                            Console.WriteLine("{0} {1}", model.FirstName, model.LastName);
+                        }
+                    }
+                    else
+                        Console.WriteLine("No data found");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
     }
 }
